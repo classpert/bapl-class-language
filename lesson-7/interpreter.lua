7ee33508e849b4db53198e2795652b92f52f81c1
Thiago Brandão <194487+thiagobrandam@users.noreply.github.com>
2023-02-02 18:53:53 -0300

    Add lesson-7

---
+++ lesson-7/interpreter.lua
@@ -0,0 +1,404 @@
+
+local lpeg = require "lpeg"
+local pt = require "pt"
+
+----------------------------------------------------
+local function I (msg)
+  return lpeg.P(function () print(msg); return true end)
+end
+
+
+local function node (tag, ...)
+  local labels = table.pack(...)
+  local params = table.concat(labels, ", ")
+  local fields = string.gsub(params, "(%w+)", "%1 = %1")
+  local code = string.format(
+    "return function (%s) return {tag = '%s', %s} end",
+    params, tag, fields)
+  return assert(load(code))()
+end
+
+----------------------------------------------------
+local function nodeSeq (st1, st2)
+  if st2 == nil then
+    return st1
+  else
+    return {tag = "seq", st1 = st1, st2 = st2}
+  end
+end
+
+local alpha = lpeg.R("AZ", "az")
+local digit = lpeg.R("09")
+local alphanum = alpha + digit
+
+local comment = "#" * (lpeg.P(1) - "\n")^0
+
+
+local maxmatch = 0
+local space = lpeg.V"space"
+
+
+local numeral = lpeg.R("09")^1 / tonumber /
+                     node("number", "val")  * space
+
+local reserved = {"return", "if", "else", "while",
+                  "new", "function"}
+local excluded = lpeg.P(false)
+for i = 1, #reserved do
+  excluded = excluded + reserved[i]
+end
+excluded = excluded * -alphanum
+
+local ID = lpeg.V"ID"
+local var = ID / node("variable", "var")
+
+
+local function T (t)
+  return t * space
+end
+
+
+local function Rw (t)
+  assert(excluded:match(t))
+  return t * -alphanum * space
+end
+
+
+local opA = lpeg.C(lpeg.S"+-") * space
+local opM = lpeg.C(lpeg.S"*/") * space
+
+
+-- Convert a list {n1, "+", n2, "+", n3, ...} into a tree
+-- {...{ op = "+", e1 = {op = "+", e1 = n1, n2 = n2}, e2 = n3}...}
+local function foldBin (lst)
+  local tree = lst[1]
+  for i = 2, #lst, 2 do
+    tree = { tag = "binop", e1 = tree, op = lst[i], e2 = lst[i + 1] }
+  end
+  return tree
+end
+
+
+local function foldIndex (lst)
+  local tree = lst[1]
+  for i = 2, #lst do
+    tree = { tag = "indexed", array = tree, index = lst[i] }
+  end
+  return tree
+end
+
+
+local lhs = lpeg.V"lhs"
+local call = lpeg.V"call"
+local factor = lpeg.V"factor"
+local term = lpeg.V"term"
+local exp = lpeg.V"exp"
+local stat = lpeg.V"stat"
+local stats = lpeg.V"stats"
+local block = lpeg.V"block"
+local funcDec = lpeg.V"funcDec"
+
+grammar = lpeg.P{"prog",
+  prog = space * lpeg.Ct(funcDec^1) * -1,
+
+  funcDec = Rw"function" * ID * T"(" * T")" * block
+              / node("function", "name", "body"),
+
+  stats = stat * (T";" * stats)^-1 / nodeSeq,
+
+  block = T"{" * stats * T";"^-1 * T"}" / node("block", "body"),
+
+  stat = block
+       + Rw"if" * exp * block * (Rw"else" * block)^-1
+           / node("if1", "cond", "th", "el")
+       + Rw"while" * exp * block / node("while1", "cond", "body")
+       + call
+       + lhs * T"=" * exp / node("assgn", "lhs", "exp")
+       + Rw"return" * exp / node("ret", "exp"),
+
+  lhs = lpeg.Ct(var * (T"[" * exp * T"]")^0) / foldIndex,
+
+  call = ID * T"(" * T")" / node("call", "fname"),
+
+  factor = Rw"new" * T"[" * exp * T"]" / node("new", "size")
+         + numeral
+         + T"(" * exp * T")"
+         + call
+         + lhs,
+
+  term = lpeg.Ct(factor * (opM * factor)^0) / foldBin,
+
+  exp = lpeg.Ct(term * (opA * term)^0) / foldBin,
+
+  space = (lpeg.S(" \t\n") + comment)^0
+            * lpeg.P(function (_,p)
+                       maxmatch = math.max(maxmatch, p);
+                       return true
+                     end),
+
+  ID = (lpeg.C(alpha * alphanum^0) - excluded) * space
+}
+
+
+local function syntaxError (input, max)
+  io.stderr:write("syntax error\n")
+  io.stderr:write(string.sub(input, max - 10, max - 1),
+        "|", string.sub(input, max, max + 11), "\n")
+end
+
+local function parse (input)
+  local res = grammar:match(input)
+  if (not res) then
+    syntaxError(input, maxmatch)
+    os.exit(1)
+  end
+  return res
+end
+
+----------------------------------------------------
+local Compiler = { funcs = {}, vars = {}, nvars = 0 }
+
+function Compiler:addCode (op)
+  local code = self.code
+  code[#code + 1] = op
+end
+
+
+local ops = {["+"] = "add", ["-"] = "sub",
+             ["*"] = "mul", ["/"] = "div"}
+
+
+function Compiler:var2num (id)
+  local num = self.vars[id]
+  if not num then
+    num = self.nvars + 1
+    self.nvars = num
+    self.vars[id] = num
+  end
+  return num
+end
+
+
+function Compiler:currentPosition ()
+  return #self.code
+end
+
+
+function Compiler:codeJmpB (op, label)
+  self:addCode(op)
+  self:addCode(label)
+end
+
+
+function Compiler:codeJmpF (op)
+  self:addCode(op)
+  self:addCode(0)
+  return self:currentPosition()
+end
+
+
+function Compiler:fixJmp2here (jmp)
+  self.code[jmp] = self:currentPosition()
+end
+
+
+function Compiler:codeCall (ast)
+  local func = self.funcs[ast.fname]
+  if not func then
+    error("undefined function " .. fname)
+  end
+  self:addCode("call")
+  self:addCode(func.code)
+end
+
+
+function Compiler:codeExp (ast)
+  if ast.tag == "number" then
+    self:addCode("push")
+    self:addCode(ast.val)
+  elseif ast.tag == "call" then
+    self:codeCall(ast)
+  elseif ast.tag == "variable" then
+    self:addCode("load")
+    self:addCode(self:var2num(ast.var))
+  elseif ast.tag == "indexed" then
+    self:codeExp(ast.array)
+    self:codeExp(ast.index)
+    self:addCode("getarray")
+  elseif ast.tag == "new" then
+    self:codeExp(ast.size)
+    self:addCode("newarray")
+  elseif ast.tag == "binop" then
+    self:codeExp(ast.e1)
+    self:codeExp(ast.e2)
+    self:addCode(ops[ast.op])
+  else error("invalid tree")
+  end
+end
+
+
+function Compiler:codeAssgn (ast)
+  local lhs = ast.lhs
+  if lhs.tag == "variable" then
+    self:codeExp(ast.exp)
+    self:addCode("store")
+    self:addCode(self:var2num(lhs.var))
+  elseif lhs.tag == "indexed" then
+    self:codeExp(lhs.array)
+    self:codeExp(lhs.index)
+    self:codeExp(ast.exp)
+    self:addCode("setarray")
+  else error("unkown tag")
+  end
+end
+
+
+function Compiler:codeBlock (ast)
+  self:codeStat(ast.body)
+end
+
+
+function Compiler:codeStat (ast)
+  if ast.tag == "assgn" then
+    self:codeAssgn(ast)
+  elseif ast.tag == "call" then
+    self:codeCall(ast)
+    self:addCode("pop")
+    self:addCode(1)
+  elseif ast.tag == "block" then
+    self:codeBlock(ast)
+  elseif ast.tag == "seq" then
+    self:codeStat(ast.st1)
+    self:codeStat(ast.st2)
+  elseif ast.tag == "ret" then
+    self:codeExp(ast.exp)
+    self:addCode("ret")
+  elseif ast.tag == "while1" then
+    local ilabel = self:currentPosition()
+    self:codeExp(ast.cond)
+    local jmp = self:codeJmpF("jmpZ")
+    self:codeStat(ast.body)
+    self:codeJmpB("jmp", ilabel)
+    self:fixJmp2here(jmp)
+  elseif ast.tag == "if1" then
+    self:codeExp(ast.cond)
+    local jmp = self:codeJmpF("jmpZ")
+    self:codeStat(ast.th)
+    if ast.el == nil then
+      self:fixJmp2here(jmp)
+    else
+      local jmp2 = self:codeJmpF("jmp")
+      self:fixJmp2here(jmp)
+      self:codeStat(ast.el)
+      self:fixJmp2here(jmp2)
+    end
+  else error("invalid tree")
+  end
+end
+
+
+function Compiler:codeFunction (ast)
+  local code = {}
+  self.funcs[ast.name] = { code = code }
+  self.code = code
+  self:codeStat(ast.body)
+  self:addCode("push")
+  self:addCode(0)
+  self:addCode("ret")
+end
+
+
+local function compile (ast)
+  for i = 1, #ast do
+    Compiler:codeFunction(ast[i])
+  end
+  local main = Compiler.funcs["main"]
+  if not main then
+    error("no function 'main'")
+  end
+  return main.code
+end
+
+----------------------------------------------------
+
+local function run (code, mem, stack, top)
+  local pc = 1
+  while true do
+  --[[
+  io.write("--> ")
+  for i = 1, top do io.write(stack[i], " ") end
+  io.write("\n", code[pc], "\n")
+  --]]
+    if code[pc] == "ret" then
+      return top
+    elseif code[pc] == "call" then
+      pc = pc + 1
+      local code = code[pc]
+      top = run(code, mem, stack, top)
+    elseif code[pc] == "pop" then
+      pc = pc + 1
+      top = top - code[pc]
+    elseif code[pc] == "push" then
+      pc = pc + 1
+      top = top + 1
+      stack[top] = code[pc]
+    elseif code[pc] == "add" then
+      stack[top - 1] = stack[top - 1] + stack[top]
+      top = top - 1
+    elseif code[pc] == "sub" then
+      stack[top - 1] = stack[top - 1] - stack[top]
+      top = top - 1
+    elseif code[pc] == "mul" then
+      stack[top - 1] = stack[top - 1] * stack[top]
+      top = top - 1
+    elseif code[pc] == "div" then
+      stack[top - 1] = stack[top - 1] / stack[top]
+      top = top - 1
+    elseif code[pc] == "load" then
+      pc = pc + 1
+      local id = code[pc]
+      top = top + 1
+      stack[top] = mem[id]
+    elseif code[pc] == "store" then
+      pc = pc + 1
+      local id = code[pc]
+      mem[id] = stack[top]
+      top = top - 1
+    elseif code[pc] == "newarray" then
+      local size = stack[top]
+      stack[top] = { size = size }
+    elseif code[pc] == "getarray" then
+      local array = stack[top - 1]
+      local index = stack[top]
+      stack[top - 1] = array[index]
+      top = top - 1
+    elseif code[pc] == "setarray" then
+      local array = stack[top - 2]
+      local index = stack[top - 1]
+      local value = stack[top]
+      array[index] = value
+      top = top - 3
+    elseif code[pc] == "jmp" then
+      pc = code[pc + 1]
+    elseif code[pc] == "jmpZ" then
+      pc = pc + 1
+      if stack[top] == 0 or stack[top] == nil then
+        pc = code[pc]
+      end
+      top = top - 1
+    else error("unknown instruction " .. code[pc])
+    end
+    pc = pc + 1
+  end
+end
+
+
+local input = io.read("a")
+local ast = parse(input)
+print(pt.pt(ast))
+local code = compile(ast)
+print(pt.pt(code))
+local stack = {}
+local mem = {}
+run(code, mem, stack, 0)
+print(stack[1])
---
+++ lesson-7/pt.lua
@@ -0,0 +1,18 @@
+local function pt (x, id, visited)
+  visited = visited or {}
+  id = id or ""
+  if type(x) == "string" then return "'" .. tostring(x) .. "'"
+  elseif type(x) ~= "table" then return tostring(x)
+  elseif visited[x] then return "..."    -- cycle
+  else
+    visited[x] = true
+    local s = id .. "{\n"
+    for k,v in pairs(x) do
+      s = s .. id .. tostring(k) .. " = " .. pt(v, id .. "  ", visited) .. ";\n"
+    end
+    s = s .. id .. "}"
+    return s
+  end
+end
+
+return {pt=pt}
